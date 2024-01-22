e1f24dbfa074e5b28676f51e58da8bf99312841b
Matt Chang <echoedinvoker@gmail.com>
2024-01-22 15:10:25 +0800

    feat: scss preprocess

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	h1 {
+		color: red;
+		font-size: functions.toRem(32);
+		@include breakpoint.up('md') {
+			color: blue;
+		}
+		@include breakpoint.up('lg') {
+			color: green;
+		}
+	}
+</style>
