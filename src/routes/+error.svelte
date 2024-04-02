99d01e3bbaa6f31641b752a789bb7ee6dbb470fe
Matt Chang <echoedinvoker@gmail.com>
2024-04-02 16:56:23 +0800

    5: markup for 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { Button } from '$components';
 </script>
 
 <svelte:head>
@@ -8,4 +9,12 @@
 
 <div class="content">
 	<h1>{$page.error?.message}</h1>
+
+	{#if $page.status === 404}
+		<p>The page you are looking for does not exist.</p>
+		<div class="buttons">
+			<Button element="a" href="/">Home</Button>
+			<Button element="a" href="/search">Search</Button>
+		</div>
+	{/if}
 </div>
