87ca72e334387a3858ab550a830f1569ea55e4cc
Matt Chang <echoedinvoker@gmail.com>
2024-04-02 16:52:29 +0800

    4: create +error.svelte for all routes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.status}</title>
+</svelte:head>
+
+<div class="content">
+	<h1>{$page.error?.message}</h1>
+</div>
