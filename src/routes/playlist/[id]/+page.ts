d2ebf016000871c398bb35fdde66aa2157932bf6
Matt Chang <echoedinvoker@gmail.com>
2024-04-08 16:08:48 +0800

    3: fetch playlist with [id]

--- src/routes/playlist/[id]/+page.ts
+++ src/routes/playlist/[id]/+page.ts
@@ -1,7 +1,16 @@
 import { fetchRefresh } from "$helpers";
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = ({ fetch: _fetch, params }) => {
+export const load: PageLoad = async ({ fetch: _fetch, params }) => {
   const fetch = (path: string) => fetchRefresh(_fetch, path)
+
+  const playlistRes = await fetch(`/api/spotify/playlists/${params.id}`)
+  if (!playlistRes.ok) {
+    throw error(playlistRes.status as any, 'Failed to fetch playlist')
+  }
+  const playlist: SpotifyApi.SinglePlaylistResponse = await playlistRes.json()
+
+  console.log(playlist)
 
 }
